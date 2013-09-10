f697243009a328535b7fe81e2333d88a2155b93c
Kevin O'Brien <keo@kevineobrien.com>
2013-09-10 12:04:22 -0500

    version 0.2
    adding radios, cleaning up code, simplifying default styles, merging
    shared js methods, adding local copy of jQuery for demo page

--- jquery.fancy_forms.js
+++ jquery.fancy_forms.js
@@ -6,7 +6,7 @@ Company: Clockwork Acive Media Systems
 Company Site: clockwork.net
 License: MIT
 Copyright (C) 2012 Clockwork Active Media Systems
-Version: 0.1
+Version: 0.2
 **************************************/
 
 (function ($) {
@@ -16,173 +16,118 @@ Version: 0.1
 	// These locally scoped variables help minification by aliasing strings
 	var data_key               =  'CW_fancy_forms',
 		event_suffix           =  '.CW_fancy_forms',
+		class_prefix           =  'fancy-',
 	// Custom events
 		component_initialized  =  'component_initialized' + event_suffix,
-		component_error        =  'component_error'       + event_suffix,
-		event_change           =  'fancy_change'          + event_suffix;
+		$elements              =  $(), // Used to filter propHooks to plugin elements only
+		$radios                =  $(); // Used to make the radio updater faster
 
 	var methods  =  {
 
+		_init_el  :  function (settings) {
 
-		/* Selects
-		*****************/
-		_init_selects  :  function (settings) {
-
-			if($(this).hasClass('fancy_selects-active')) { return this; }
+			if($(this).hasClass(class_prefix + 'active')) { return this; }
 
 			var $el  =  $(this),
+				type =  $el.prop("tagName").toLowerCase(),
+				type =  type === 'select' ? type : $el.attr('type'),
 				options  =  $.extend({
-					content         :  null,
-					selected        :  $el.find('option:selected'),
-					wrap_class      :  'fancy_selects-wrap',
-					// consider a private function
-					wrap_id         :  $el.attr('id') ? 'fancy_selects-' + $el.attr('id') : '',
+					// Settings for selects, checkboxes and radios
+					selected_class  :  class_prefix + type + '-checked',
+					wrap_class      :  class_prefix + type +'-wrap',
+					wrap_id         :  $el.attr('id') ? class_prefix + type +'-' + $el.attr('id') : '',
 					wrap_tag        :  'div',
-					visible_class   :  'fancy_selects-visible',
-					visible_tag     :  'div'
+					type            :  type
 				}, settings);
-
-			var visible_content  =  options.content || options.selected.html();
 			options.wrap_html       =  '<' + options.wrap_tag + ' class="' + options.wrap_class + '" id="' + options.wrap_id + '"></' + options.wrap_tag + '>';
-			options.visible_html   =  '<' + options.visible_tag + ' class="' + options.visible_class + '">' + visible_content + '</' + options.visible_tag + '>';
+			$elements = $elements.add(this);
+			
+			if(type === 'select') {
+				// Settings specific to selects
+				options  =  $.extend({
+					selected        :  $el.find('option:selected'),
+					visible_class   :  class_prefix + type +'-visible',
+					fallback        :  '<div class="'+ class_prefix + type +'-fallback_button"></div>',
+					visible_tag     :  'div'
+				}, options);
+				options.visible_html   =  '<' + options.visible_tag + ' class="' + options.visible_class + '">' + options.selected.html() + '</' + options.visible_tag + '>';
+			}else {
+				// Makes sure change event is fired when jQuery change is called on element
+				$.propHooks.checked = {
+					set: function(el, value) {
+						if ($elements.filter(el).length !== 0) {
+							el.checked = value;
+							$(el).trigger('change');
+						}
+					}
+				};
+				if(type === 'radio') {
+					$radios = $radios.add(this);
+				}
+			}
 
 			$el.data(data_key, options);
-
-			methods._make_fancy_select($el);
+			methods._make.apply(this);
 
 			return this;
 		},
 
+		_make  :  function (evt) {
 
-		_make_fancy_select  :  function ($el) {
+			var	$el      =  $(this),
+				options  =  evt ? $(evt.currentTarget).data(data_key) : $(this).data(data_key);
 
-			var	options  =  $el.data(data_key);
-
-			$el.wrap(options.wrap_html).addClass('fancy_selects-active');
-
-			options.$wrap     =  $el.parent();
-			options.$visible  =  $(options.visible_html).appendTo(options.$wrap);
-			if ($.browser.msie  && parseInt($.browser.version, 10) < 8) {
-				$('<div class="fancy_selects-fallback_button">&nu;</div>').appendTo(options.$wrap);
-			}
-
-			methods.apply_fancy_css($el, options.$wrap);
-
-			$el
-				.change(methods.update_selected_select)
-				.on(event_change, options.$wrap, methods.update_selected_select);
-
-			$el.trigger(component_initialized, options.$wrap, options);
-		},
-
-
-		update_selected_select  :  function (evt) {
-
-			var	$el      =  $(evt.currentTarget),
-				options  =  $el.data(data_key);
-
-			if(options.$visible) {
-				options.selected  =  options.$wrap.find('option:selected');
-				options.$visible.html(options.selected.html());
-			}
-
-		},
-
-		/* Shared
-		*****************/
-		apply_fancy_css  :  function($el, $wrapper) {
-
-			$wrapper
+			options.$wrap = $el.wrap(options.wrap_html).parent()
 				.css({
-					'position'       :  'relative',
-					'width'          :  $el.outerWidth() + 'px'
+					'width'          :  $el.outerWidth() + 'px',
+					'height'          :  $el.outerHeight() + 'px'
 				})
-				.addClass($el.attr('class'))
+				.addClass(data_key + '-' + $el.attr('class'))
 				.attr('id', data_key + '-' + $el.attr('id' ));
 
-			$el.css({
-				'opacity'  :  0,
-				'min-height'  :  '100%',
-				'min-width'   :  '100%',
-				'position'    :  'absolute',
-				'top'         :  0,
-				'left'        :  0,
-				'z-index'     :  2,
-				'cursor'      :  'pointer'
-			});
-		},
+			if(options.type === 'select') {
+				options.$visible  =  $(options.visible_html).appendTo(options.$wrap);
+				if ($.browser.msie  && parseInt($.browser.version, 10) < 8) {
+					$(options.fallback).appendTo(options.$wrap);
+				}
+			}else if(options.type === 'radio') {
+				// Need to check all since checking one in a group doesn't fire change on the element that looses checked
+				$el.change(function(){ $radios.each(function(){ methods.update.apply(this) }); });
+			}
 
-		/* Checkboxes
-		*****************/
-		_init_checks  :  function (settings) {
-
-			var $el  =  $(this),
-				options  =  $.extend({
-					selected_class  :  'fancy_checkbox-checked',
-					wrap_class      :  'fancy_checkbox-wrap',
-					wrap_id         :  $el.attr('id') ? 'fancy_checkbox-' + $el.attr('id') : '',
-					wrap_tag        :  'div'
-				}, settings);
-
-			options.wrap_html       =  '<' + options.wrap_tag + ' class="' + options.wrap_class + '" id="' + options.wrap_id + '"></' + options.wrap_tag + '>';
-
-			$el.data(data_key, options);
-
-			methods._make_fancy_checks($el);
+			$el
+				.change(methods.update)
+				.each(methods.update)
+				.trigger(component_initialized, options.$wrap, options)
+				.addClass(class_prefix +'active');
 
 			return this;
 		},
 
+		/* usage: jQuery( element ).cw_fancy_forms("update") */
+		update  :  function (evt) {
 
-		_make_fancy_checks  :  function ($el) {
-
-			var	options  =  $el.data(data_key);
-
-			$el
-				.wrap(options.wrap_html)
-				.change(methods.update_selected_check)
-				.each(methods.update_selected_check)
-				.trigger(component_initialized, options);
-			
-			options.$wrap = $el.parent();
-
-			methods.apply_fancy_css($el, $el.parent());
-		},
-
-
-		update_selected_check  :  function (evt) {
-
-			var	options  =  evt ? $(evt.currentTarget).data(data_key) : $(this).data(data_key),
-				$el   =  $(this),
+			var	$el      =  $(this),
+				options  =  evt ? $(evt.currentTarget).data(data_key) : $(this).data(data_key),
 				checked  =  $el.prop('checked');
 
-			if(checked === true) {
-				$el.parent().addClass(options.selected_class);
+			if((options.type === 'select') && options.$visible) {
+				options.selected  =  options.$wrap.find('option:selected');
+				options.$visible.html(options.selected.html());
 			}else {
-				$el.parent().removeClass(options.selected_class);
+				checked === true ? $el.parent().addClass(options.selected_class) : $el.parent().removeClass(options.selected_class);
 			}
 
-			$el.trigger(event_change, options);
 		}
 	};
 
-	$.fn.cw_fancy_selects  =  function (method) {
+	$.fn.cw_fancy_forms  =  function (method) {
 
 		if (methods[method] && method.charAt(0) !== '_') {
 			return $(this).map(function(i, val) { return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); });
 		} else if (typeof method === 'object' || !method) {
 			var args  =  arguments;
-			return $(this).map(function(i, val) { return methods._init_selects.apply(this, args); });
-		}
-	};
-
-	$.fn.cw_fancy_checks  =  function (method) {
-
-		if (methods[method] && method.charAt(0) !== '_') {
-			return $(this).map(function(i, val) { return methods[method].apply(this, Array.prototype.slice.call(arguments, 1)); });
-		} else if (typeof method === 'object' || !method) {
-			var args  =  arguments;
-			return $(this).map(function(i, val) { return methods._init_checks.apply(this, args); });
+			return $(this).map(function(i, val) { return methods._init_el.apply(this, args); });
 		}
 	};
 
